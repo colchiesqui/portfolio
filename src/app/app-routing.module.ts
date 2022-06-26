2af6f576ccad5af27e140910a12e14027ac4c7f1
Pablo Colchiesqui <pablocolchiesqui@vectronic-aerospace.com>
2022-06-26 23:00:37 +0200

    feat: updates routing to use the correct components for each page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { AboutMePageComponent } from './pages/about-me-page/about-me-page.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { TechnologiesPageComponent } from './pages/technologies-page/technologies-page.component';
+import { TestimonialsPageComponent } from './pages/testimonials-page/testimonials-page.component';
 
-const routes: Routes = [];
+const routes = [
+    { path: '', component: AboutMePageComponent },
+    { path: 'about-me', component: AboutMePageComponent },
+    { path: 'technologies', component: TechnologiesPageComponent },
+    { path: 'portfolio', component: PortfolioPageComponent },
+    { path: 'testimonials', component: TestimonialsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
